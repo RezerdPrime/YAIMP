177454e8cd36bff0cdd5c71ecfb199e4e2ff0ef0
RezerdPrime <101434848+RezerdPrime@users.noreply.github.com>
2022-09-27 19:05:54 +1000

    Create 1_10_253.cpp

---
+++ 1_10_253.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+    cin >> year;
+
+    if ((year%4==0) and ((year%400==0) or (year%100!=0))){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
