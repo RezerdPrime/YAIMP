e28d5940358e9a59692bf4739600ca7670bf5f6c
RezerdPrime <101434848+RezerdPrime@users.noreply.github.com>
2022-10-01 11:08:02 +1000

    Update 1_5_125.cpp

--- 1_HW_pack/1_5_125.cpp
+++ 1_HW_pack/1_5_125.cpp
@@ -1,46 +1,17 @@
-/*
-#include <iostream>
-#include <string> //Для введения строки
-#include <cmath> //Для степени (перевода в 10сс)
-using namespace std;
-
-int main()
-{
-    int A, i, A2; string s = "", sym;
-    cin >> A >> i;
-
-    while (A>0) {
-        s += to_string(A%2); //Перевод инта в строку
-        A /= 2;
-    }
-
-    if (i >= s.length()) {
-        string str("0", i - s.length() + 1); s.append(str);
-    }
-    sym = s[i];
-    A2 = atoi(sym.c_str()); //Перевод символа в инт
-    s = s.replace(i, 1, to_string((A2 + 1) % 2)); //Инвертация бита
-
-    A = 0;
-
-    for (int j = s.length()-1; j > -1; j--) {
-        sym = s[j];
-        A += pow(2,j) * atoi(sym.c_str());
-    }
-    cout << A;
-}
-*/
-
 #include <iostream>
 using namespace std;
 
 int main() {
-    int s, x, k;
-    cin >> s >> k;
+    int A, i;
+    cin >> A >> i;
 
-    x = ~(1<<k);
-    s = s & x;
-
-    cout << s << endl;
+    cout << (A ^ (1 << i)) << endl;
 }
 
+/*
+ * При исключающем ИЛИ происходит следующее изменение битов:
+ *      1 ^ 0 = 1
+ *      1 ^ 1 = 0
+ *      0 ^ 0 = 0
+ *      0 ^ 1 = 1
+ */
