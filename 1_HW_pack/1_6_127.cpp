ccfa01124c60b477a22e42c21b991588051516fc
RezerdPrime <101434848+RezerdPrime@users.noreply.github.com>
2022-10-01 11:13:13 +1000

    Update 1_6_127.cpp

--- 1_HW_pack/1_6_127.cpp
+++ 1_HW_pack/1_6_127.cpp
@@ -1,28 +1,17 @@
 #include <iostream>
-#include <string> //Для введения строки
-#include <cmath> //Для степени двойки
 using namespace std;
 
-int main()
-{
-    int A, n; string s = "", s2;
+int main() {
+    int A, n;
     cin >> A >> n;
-
-    while (A>0) {
-        s += to_string(A%2); //Перевод инта в строку
-        A /= 2;
-    }
-
-    if (n > s.length()) { //Защита от выхода за диапазон
-        n = s.length();
-    }
-
-    s = s.substr(0, n);
-    A = 0;
-
-    for (int i = s.length()-1; i > -1; i--) {
-        s2 = s[i];
-        A += pow(2,i) * atoi(s2.c_str());
-    }
-    cout << A;
+  cout << (A & ~((A >> n) << n)) << endl;
 }
+
+/*
+
+10111 >> <<
+10100 ~
+01011 &
+=> 00011
+
+*/
