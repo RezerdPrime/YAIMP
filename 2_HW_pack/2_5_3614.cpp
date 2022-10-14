f5e08ee38ab10143a42f89acf4655e9fdb023f0c
RezerdPrime <101434848+RezerdPrime@users.noreply.github.com>
2022-10-15 00:34:01 +1000

    Create 2_5_3614.cpp

---
+++ 2_HW_pack/2_5_3614.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float degrees; cin >> degrees;
+    int degrees_int;
+
+    int hour, min, sec;
+    cout << degrees * 120 << endl;///
+    degrees_int = int(degrees * 120); degrees_int %= 43200;
+
+    cout << degrees_int << endl;///
+
+    hour = degrees_int / 3600; degrees_int -= hour * 3600;
+    min = degrees_int / 60; degrees_int -= min * 60;
+    sec = degrees_int;
+
+    cout << hour << " " << min << " " << sec << endl;
+    cout << hour * 3600 + min * 60 + sec << endl;///
+}
+
+
+// 30 deg = 1 hour      1 hour = 3600 sec   =>  1 deg = 120 sec
+// 31 deg = 31 * 120 = 3720 sec
